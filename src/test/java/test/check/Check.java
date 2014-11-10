2ffbc15008d24b89d2b2b13ad025592a06a8050f
Pawan Singh <pawan.singh@gspann.com>
2014-11-10 11:43:54 +0530

    ok

--- src/test/java/test/check/Check.java
+++ src/test/java/test/check/Check.java
@@ -1,13 +1,35 @@
 package test.check;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Check 
 {
-	@Test
-	public void display()
+	private WebDriver driver;
+	private String google;
+	@BeforeSuite
+	public void initialize()
 	{
-		System.out.println("$$$$$$Check$$$$$$");
+		driver=new FirefoxDriver();
+		google="https://www.google.co.in/";
+	}
+	@Test
+	public void display() throws InterruptedException
+	{
+		driver.get(google);
+		
+		JavascriptExecutor js=(JavascriptExecutor)driver; 	
+		js.executeScript("alert('Hi, Welcome to google');");
+		Thread.sleep(2000);
+		Alert alert=driver.switchTo().alert();
+		alert.accept();
+		System.out.println(driver.getCurrentUrl());
+		
+		
 	}
 
 }
